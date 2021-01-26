da96d587ca29385fa2dd5951f06f4eb6124591d5
matthewlazewski <matthew.lazewski@gmail.com>
2021-01-26 13:52:38 -0600

    added youtube video links to projects

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -13,6 +13,7 @@ const Projects = ({ user }) => {
                         <ProjectItem key={i}>
                           <a href={project.githubUrl}><ProjectTitle>{project.name}</ProjectTitle></a>
                           <p>{project.state}</p> 
+                            <a href={project.videos.map(video => Object.values(video.url).join(''))} name={project.name}>{project.videos.map(video => Object.values(video.url).join(''))} name={project.name}</a>
                           <SkillContainer>
                               {[...project.languages, ...project.libraries].map((item, j) => (
                                   <Pill key={j}>{item}</Pill>
